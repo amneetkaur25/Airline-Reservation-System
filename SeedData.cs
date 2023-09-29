c61559607e93c261e2c55d80f64ddb657c8ae553
Amneet Kaur <111182139+amneetkaur25@users.noreply.github.com>
2023-09-29 12:15:18 +0530

    Add files via upload

---
+++ SeedData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Airline_Resevation_System.Seeder
+{
+    public class SeedData
+    {
+        public static async Task SeedAdminUserAsync(UserManager<IdentityUser> userManager)
+        {
+            if(await userManager.FindByEmailAsync("admin@gmail.com") == null)
+            {
+                var adminUser = new IdentityUser
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(adminUser, "Admin123!");
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+    }
+}
